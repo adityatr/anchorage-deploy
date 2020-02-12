56a47d47f9b55ce9f53c79a450830d6ed366ebaa
aditya <huskywhale@gmail.com>
2020-02-11 21:23:34 -0500

    add init

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import router from './router'
+import { createProvider } from './vue-apollo'
+
+Vue.config.productionTip = false
+
+new Vue({
+  store,
+  router,
+  apolloProvider: createProvider(),
+  render: h => h(App)
+}).$mount('#app')
